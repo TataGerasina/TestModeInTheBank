59e21bee921190a346af0d24ddf57dc596109a9b
Tatyana Gerasina <gtstata@gmail.com>
2024-06-25 13:05:03 +0300

    Update project

--- src/test/java/ru/netology/test/ModeInTheBankTest.java
+++ src/test/java/ru/netology/test/ModeInTheBankTest.java
@@ -7,8 +7,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
-import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static ru.netology.data.DataGenerator.Registration.getRegisteredUser;
